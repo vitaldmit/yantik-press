c70dd40b6128ca3c63c22f369d077891551ca682
Заведующий сектором информатизации <yantik_info@cap.ru>
2020-03-25 12:53:30 +0300

    Подсветка родительского меню

--- static/js/base.js
+++ static/js/base.js
@@ -5,11 +5,17 @@
   });
 })( jQuery );
 
+// Карусель для первого поста на главной
 ;$('.carousel').carousel({
   interval: 3000
 });
 
+// Подсветка меню
 ;$(document).ready(function(){
   var link = window.location.pathname;
   $('a[href="'+link+'"]').addClass('active');
+  if(link.startsWith('/news/'))
+    $('a[href="/news/"]').addClass('active');
+  if(link.startsWith('/about/'))
+    $('a[href="/about/"]').addClass('active');
 });
